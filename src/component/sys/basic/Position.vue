dd7fe78fe7fe48ab0fc5049071c412f79844ec4b
zhayh <zhayh.nj@gmail.com>
2020-04-22 20:53:28 +0800

    职位管理的批量删除功能

--- src/component/sys/basic/Position.vue
+++ src/component/sys/basic/Position.vue
@@ -7,7 +7,7 @@
       <el-button type="primary" icon="el-icon-plus" size="small" @click="addPosition">
         添加
       </el-button>
-      <el-table :data="positions" stripe border type="small" style="width: 70%">
+      <el-table :data="positions" stripe border type="small" style="width: 70%" @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="56"> </el-table-column>
         <el-table-column prop="id" label="编号" width="56"> </el-table-column>
         <el-table-column prop="name" label="职位名称" width="180"> </el-table-column>
@@ -19,6 +19,10 @@
           </template>
         </el-table-column>
       </el-table>
+      <el-button type="danger" size="small" style="margin-top: 8px" @click="deleteMany"
+                 :disabled="multipleSelection.length === 0">
+        批量删除
+      </el-button>
     </div>
     <el-dialog title="修改职位" :visible.sync="dialogVisible" width="30%">
       <div>
@@ -46,7 +50,8 @@ export default {
       updatePos: {
         name: ''
       },
-      dialogVisible: false
+      dialogVisible: false,
+      multipleSelection: []
     }
   },
   methods: {
@@ -87,6 +92,30 @@ export default {
         type: 'warning'
       }).then(() => {
         this.deleteRequest('/system/basic/pos/' + data.id).then(resp => {
+          this.initPositions()
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消操作'
+        });
+      });
+    },
+    handleSelectionChange(val) {
+      console.log(val)
+      this.multipleSelection = val
+    },
+    deleteMany() {
+      this.$confirm('此操作将永久删除' + this.multipleSelection.length + '条记录, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        let ids = "?"
+        this.multipleSelection.forEach(item => {
+          ids += "ids=" + item.id + '&'
+        })
+        this.deleteRequest('/system/basic/pos/' + ids).then(resp => {
           this.initPositions()
         })
       }).catch(() => {
